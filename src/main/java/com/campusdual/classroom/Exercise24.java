7db32d610beab1cd4195057cc66089f5a421971f
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2024-10-01 14:27:04 +0200

    Resuelto.

--- src/main/java/com/campusdual/classroom/Exercise24.java
+++ src/main/java/com/campusdual/classroom/Exercise24.java
@@ -5,14 +5,26 @@ import java.util.Queue;
 
 public class Exercise24 {
     public static Queue<String> createQueue() {
+        Queue<String> cola = new LinkedList<>();
+        cola.add("Smith");
+        cola.add("Montessori");
+        cola.add("Peralta");
+        cola.add("House");
 
+        return cola;
     }
-    public static void printAndEmptyQueue(Queue<String> queue) {
 
+    public static void printAndEmptyQueue(Queue<String> queue) {
+       int tamanoCola = queue.size();
+
+        while (tamanoCola > 0) {
+            System.out.println(queue.poll());
+            tamanoCola--;
+        }
     }
 
     public static void main(String[] args) {
-
+        Queue<String> cola = createQueue();
+        printAndEmptyQueue(cola);
     }
-
 }
